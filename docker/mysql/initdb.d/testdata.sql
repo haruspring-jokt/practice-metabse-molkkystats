0a1c759c73fd9d29436f913bdc7b9ae0f194855c
haruspring-jokt <h0534jokt@gmail.com>
2019-12-03 23:27:49 +0900

    モルック棒の種類が増えたよ

--- docker/mysql/initdb.d/testdata.sql
+++ docker/mysql/initdb.d/testdata.sql
@@ -1,4 +1,6 @@
 INSERT INTO color (id, color, name, kind, weight) VALUES (1, 'RED', '赤', 'SET', 356);
 INSERT INTO color (id, color, name, kind, weight) VALUES (2, 'YELLOW', '黄', 'LOGO', 345);
 INSERT INTO color (id, color, name, kind, weight) VALUES (3, 'PINK', 'ピンク', 'LOGO', 333);
-INSERT INTO color (id, color, name, kind, weight) VALUES (4, 'BLUE', '青', 'LOGO', 333);
+INSERT INTO color (id, color, name, kind, weight) VALUES (4, 'BLUE', '青', 'LOGO', 339);
+INSERT INTO color (id, color, name, kind, weight) VALUES (5, 'WHITE', '白', 'LOGO', 333);
+INSERT INTO color (id, color, name, kind, weight) VALUES (6, 'BLACK', '黒', 'LOGO', 363);

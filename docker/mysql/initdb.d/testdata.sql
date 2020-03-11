6dab1236955c2468cc4cd8dd38013022560a67f4
haruspring-jokt <h0534jokt@gmail.com>
2020-03-12 00:51:05 +0900

    バックアップ取得

--- docker/mysql/initdb.d/testdata.sql
+++ docker/mysql/initdb.d/testdata.sql
@@ -4,3 +4,5 @@ INSERT INTO color (id, color, name, kind, weight) VALUES (3, 'PINK', 'ピンク'
 INSERT INTO color (id, color, name, kind, weight) VALUES (4, 'BLUE', '青', 'LOGO', 339);
 INSERT INTO color (id, color, name, kind, weight) VALUES (5, 'WHITE', '白', 'LOGO', 333);
 INSERT INTO color (id, color, name, kind, weight) VALUES (6, 'BLACK', '黒', 'LOGO', 363);
+INSERT INTO color (id, color, name, kind, weight) VALUES (7, 'PURPLE', '紫', 'LOGO', 375);
+INSERT INTO color (id, color, name, kind, weight) VALUES (8, 'WHITE2', '白2', 'LOGO', 365);
